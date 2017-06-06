cdd9026a35be40a1fcbc7657fed89d02ecdb7a95
Michael Müller <michael@fds-team.de>
2017-06-06 21:08:51 +0200

    ntoskrnl.exe: Implement NtBuildNumber.

--- dlls/ntoskrnl.exe/ntoskrnl.c
+++ dlls/ntoskrnl.exe/ntoskrnl.c
@@ -58,6 +58,7 @@ WINE_DECLARE_DEBUG_CHANNEL(plugplay);
 
 BOOLEAN KdDebuggerEnabled = FALSE;
 ULONG InitSafeBootMode = 0;
+USHORT NtBuildNumber = 0;
 
 extern LONG CALLBACK vectored_handler( EXCEPTION_POINTERS *ptrs );
 
@@ -3026,6 +3027,13 @@ NTSTATUS WINAPI IoAcquireRemoveLockEx(PIO_REMOVE_LOCK lock, PVOID tag,
     return STATUS_NOT_IMPLEMENTED;
 }
 
+static void ntoskrnl_init(void)
+{
+    LARGE_INTEGER count;
+
+    KeQueryTickCount( &count );  /* initialize the global KeTickCount */
+    NtBuildNumber = NtCurrentTeb()->Peb->OSBuildNumber;
+}
 
 /*****************************************************
  *           DllMain
@@ -3033,7 +3041,6 @@ NTSTATUS WINAPI IoAcquireRemoveLockEx(PIO_REMOVE_LOCK lock, PVOID tag,
 BOOL WINAPI DllMain( HINSTANCE inst, DWORD reason, LPVOID reserved )
 {
     static void *handler;
-    LARGE_INTEGER count;
 
     switch(reason)
     {
@@ -3042,7 +3049,7 @@ BOOL WINAPI DllMain( HINSTANCE inst, DWORD reason, LPVOID reserved )
 #if defined(__i386__) || defined(__x86_64__)
         handler = RtlAddVectoredExceptionHandler( TRUE, vectored_handler );
 #endif
-        KeQueryTickCount( &count );  /* initialize the global KeTickCount */
+        ntoskrnl_init();
         break;
     case DLL_PROCESS_DETACH:
         if (reserved) break;
